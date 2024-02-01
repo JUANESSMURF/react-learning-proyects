f4fb2ef5d18d09a40f0c4e9ffdebcb4b2c493650
Juan <je.ortiz10003@gmail.com>
2024-02-01 11:31:50 -0500

    Add turns to know whenever is X turns or O turns

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,19 +1,29 @@
+import { useState } from "react"
+
 const TURNS = {
   X: 'x',
   O: 'o'
 }
 
-const board = Array(9).fill(null)
 
-const Square = ({ children, updateBoard, index }) => {
+
+const Square = ({ children, isSelected, updateBoard, index }) => {
+  const className = `square ${isSelected ? 'is-selected' :''} `
+  
   return (
-    <div className="square">
+    <div className={className}>
       {children}
     </div>
   )
 }
 
 function App() {
+  const [board, setBoard] = useState(
+    Array(9).fill(null)
+    )
+  const [turn, setTurn] = useState(TURNS.X)
+
+
   return (
     <main className='board'>
       <h1>Tic Tac Toe</h1>
@@ -25,11 +35,19 @@ function App() {
               key={index}
               index={index}
             >
-              {index}
+              {board[index]}
             </Square> 
           )
         })
       }
+      </section>
+      <section className="turn">
+        <Square isSelected={turn === TURNS.X}>
+          {TURNS.X}
+        </Square>
+        <Square isSelected={turn === TURNS.O}>
+          {TURNS.O}
+        </Square>
       </section>
     </main>
     )
